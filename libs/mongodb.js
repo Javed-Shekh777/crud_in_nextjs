b2ecce79fc8195bc02b67e5d50955afa34980313
javed-shekh777 <mdjavedshekh777>
2025-03-12 11:25:59 +0530

    first

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const conectMongoDB =async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}${process.env.DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log(`Database connected to `);
   } catch (error) {
     console.log(`Db connection error : ${error}`);
